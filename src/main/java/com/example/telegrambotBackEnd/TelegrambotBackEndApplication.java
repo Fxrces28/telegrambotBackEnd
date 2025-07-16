af02218df83ebeb113ce1ee1d44d29a4764b90d8
Fxrces28 <cr33p.2004@gmail.com>
2025-07-16 21:15:12 +0300

    Создание backend проекта

---
+++ src/main/java/com/example/telegrambotBackEnd/TelegrambotBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/telegrambotBackEnd/TelegrambotBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
